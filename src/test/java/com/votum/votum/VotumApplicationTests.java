0a92dfbac0ddbcde704ffd2757e9775396df4989
Eduar-bc <edbc192126@gmail.com>
2025-09-16 16:27:55 -0500

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/votum/votum/VotumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votum.votum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
