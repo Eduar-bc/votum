074723fe4a3144c8c96b5d60ac7141b0f1e081e0
Eduar-bc <edbc192126@gmail.com>
2025-09-17 09:33:53 -0500

    feat: Creación de tabla users inicial, agrego dependencia validator

---
+++ src/main/java/com/votum/votum/model/Role.java
@@ -0,0 +1,6 @@
+package com.votum.votum.model;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_VOTER
+}
---
+++ src/main/java/com/votum/votum/model/User.java
@@ -0,0 +1,30 @@
+package com.votum.votum.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Email(message = "Email inválido")
+    @Column(unique=true, nullable=false)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    public Set<Role> roles;
+}
