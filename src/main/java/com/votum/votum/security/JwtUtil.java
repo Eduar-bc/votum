8b38b444ce17bd6ea9d1e169e090e61cfcc23280
Eduar-bc <edbc192126@gmail.com>
2025-09-17 14:53:17 -0500

    fix: Añadido JwtUtil para la gestión de tokens de seguridad

---
+++ src/main/java/com/votum/votum/security/JwtUtil.java
@@ -0,0 +1,63 @@
+package com.votum.votum.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+    private final Key key;
+    private final long expirationMs;
+
+    public JwtUtil(@Value("${app.jwt.secret}") String secret, @Value("${app.jwt.expiration-ms}") long expirationMs) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expirationMs = expirationMs;
+    }
+
+    public String generateToken(String email) {
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .signWith(key, SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = Jwts.parser().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody();
+        return resolver.apply(claims);
+    }
+
+    public boolean validateToken(String token, String email){
+        try {
+            String user = extractUsername(token);
+            return user.equals(email) && !isTokenExpired(token);
+        }catch (JwtException | IllegalArgumentException e){
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token){
+        Date exp = extractClaim(token, Claims::getExpiration);
+        return exp.before(new Date());
+    }
+}
